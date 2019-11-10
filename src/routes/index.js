3fd11316b4b90da69155c47c8eef46768fcd88a5
Lionking <lionking@Lionkings-Mac-mini.local>
2019-11-11 05:02:40 +1000

    add store redux saga routes jsconfig

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrowserRouter as Router, Redirect, Route} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Container } from 'reactstrap'
+
+const routes = () => (
+    <Router>
+        <div>
+            Hello World!
+        </div>
+    </Router>
+)
